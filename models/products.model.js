a159072fbca2f79c6e5df090d25ff435623787dc
Saurav Kumar <maxsaurav267@gmail.com>
2021-05-06 22:09:37 +0530

    created products Schema and model

---
+++ models/products.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ProductSchema = new mongoose.Schema({
+	_id: Schema.Types.ObjectId,
+	name: String,
+  image: String,
+  price: Number,
+  brand: String,
+  inStock: Boolean,
+  fastDelivery: Boolean,
+  quantity: Number,
+  ratings: Number,
+  offer: String,
+},
+	{ timestamps: true }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = { Product };
