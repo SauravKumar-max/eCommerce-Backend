b30a25ea2eff2f6dcffe1fd0a7b99450c3e2cb34
Saurav Kumar <maxsaurav237@gmail.com>
2021-09-09 21:42:50 +0530

    updated cart route

--- routes/carts.route.js
+++ routes/carts.route.js
@@ -7,39 +7,59 @@ const { extend } = require("lodash");
 router.route("/")
 .get(async (req, res) => {
 	try{
-		let cart = await Cart.find().populate("_id");
-		cart = cart.map(item => {
-			const { _id, ..._doc } = item._id._doc;
-			 return { _id, ..._doc, quantity: item.quantity};
+		const { userId } = req.user;
+		let userCart = await Cart.findById({_id: userId}).populate("cart._id");
+
+		if(userCart !== null){
+			userCart = userCart.cart.map(item => {
+				const { _id, quantity } = item
+				return Object.assign( _id, { quantity });
 			});
-		res.json({success: true, cart});
+		} else{
+				userCart = [];
+		} 
+		res.json({success: true, userCart});
 	}catch(error){
 		console.log(error);
-	}
+}
 })
+
 
 .post(async (req, res) => {
 	try{
-		const newCartItem = req.body;
-		const AddCartItem = new Cart(newCartItem)
-		const saveCart = await AddCartItem.save();
-		res.json({success: true, saveCart});
+		const { userId } = req.user;
+	  const userRequest = req.body;
+		const findUser = await Cart.findById({ _id: userId });
+	
+		if(findUser){
+			findUser.cart.push(userRequest);
+			const updatedCart = findUser.cart;
+			await Cart.findByIdAndUpdate({_id: userId}, { cart: updatedCart });
+			res.json({ success: true, updatedCart });
+		}else{
+			const newCartItem = { _id: userId, cart: [userRequest] };
+			const newUserCart = new Cart(newCartItem);
+			const saveCart = await newUserCart.save();
+			res.json({ success: true, saveCart });
+		}
 	}catch(error){
 		console.error(error);
 	}
 })
 
+
 router.param("productId", async (req, res, next, productId) => {
 	try{
-
-		const product = await Cart.findById(productId);
-
+		const { userId } = req.user;
+		const userCart = await Cart.findById(userId);
+		const product = userCart.cart.find(product => product._id == productId);
+		
 		if(!product){
 			return res.status(400).json({success: false, message: "error in getting product"})
 		}
-
-		req.product = product;
+		req.product = { userId, userCart, product };
 		next();
+
 	}catch{
 		res.status(400).json({success: false, message: "product not found!"})
 	}
@@ -50,7 +70,7 @@ router.param("productId", async (req, res, next, productId) => {
 router.route('/:productId')
 .get( async (req, res) => {
 	try{
-		const { product } = req;
+		const { product } = req.product;
 		res.json({ success: true, product });
 	}catch(error){
 		console.error(error);
@@ -59,9 +79,15 @@ router.route('/:productId')
 
 .post(async (req, res) => {
 	try{
-		const { productId } = req.params;
+		const { userId, userCart, product } = req.product;
 		const { quantity } = req.body;
-		await Cart.findByIdAndUpdate(productId, {quantity});
+
+		const updatedCart = userCart.cart.map(item => 
+			item._id == product._id ? Object.assign(item, {quantity}) : item 
+		);
+
+		await Cart.findByIdAndUpdate({_id: userId}, {cart: updatedCart});
+
 		res.json({success: true, quantity });
 	}catch(error){
 		console.error(error);
@@ -70,12 +96,21 @@ router.route('/:productId')
 
 .delete(async (req, res) => {
 	try{
-		const { product } = req;
-		await product.remove();
-		res.json({ success: true, product});
+		const { userId, userCart, product } = req.product;
+		const { cart } = userCart;
+		if(cart.length === 1){
+			await userCart.remove();
+			res.json({ success: true, product});
+		}else{
+			const filteredCart = userCart.cart.filter(item => item._id != product._id);
+			await Cart.findByIdAndUpdate({_id: userId}, {cart: filteredCart});
+			res.json({ success: true, product});
+		}
+		
 	}catch(error){
 		console.log(error);
 	}
 })
+
 
 module.exports = router;
