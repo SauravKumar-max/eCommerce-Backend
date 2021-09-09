6209f9601dcd53181ee9bd8991c9261f3722fe1d
Saurav Kumar <maxsaurav237@gmail.com>
2021-09-09 21:42:01 +0530

    updated wishlist route for user specific

--- routes/wishlists.route.js
+++ routes/wishlists.route.js
@@ -1,53 +1,70 @@
 const express = require('express');
 const router = express.Router();
-const { Product } = require("../models/products.model");
 const { Wishlist } = require('../models/wishlists.model');
 
 
 router.route('/')
 .get( async (req, res) => {
 	try{
-		let wishlist = await Wishlist.find({}).populate('_id');
-		wishlist = wishlist.map(item => item._id);
-		res.json({ success: true, wishlist });
+		const { userId } = req.user;
+		let userWishlist = await Wishlist.findById({_id: userId}).populate('wishlist');
+		if(userWishlist !== null){
+			userWishlist = userWishlist.wishlist;
+		}else{
+			userWishlist = [];
+		}
+		res.json({ success: true, userWishlist });
 	}catch(error){
 		console.error(error);
 	}
 })
 
-.post(async (req, res) => {
+.post(async (req, res) => { 
 	try{
-		const newWishlistItem = req.body;
-		const AddWishlistItem = new Wishlist(newWishlistItem);
-		const saveWishlist = await AddWishlistItem.save();
-		res.json({ success: true, saveWishlist });
-	}catch(error){
-		console.error(error);
-	}
-})
-
-router.param("productId", async (req, res, next, productId) => {
-	try{
-
-		const product = await Wishlist.findById(productId);
-
-		if(!product){
-			return res.status(400).json({success: false, message: "error in getting product"})
+		const { userId } = req.user;
+	  const userRequest = req.body;
+		const findUser = await Wishlist.findById({ _id: userId });
+	
+		if(findUser){
+			findUser.wishlist.push(userRequest);
+			const updatedWishlist = findUser.wishlist;
+			await Wishlist.findByIdAndUpdate({_id: userId}, { wishlist: updatedWishlist });
+			res.json({ success: true, updatedWishlist: findUser });
+		}else{
+			const newWishlistItem = { _id: userId, wishlist: [userRequest] };
+			const newUserWishlist = new Wishlist(newWishlistItem);
+			const saveWishlist = await newUserWishlist.save();
+			res.json({ success: true, saveWishlist });
 		}
 
-		req.product = product;
-		next();
-	}catch{
-		res.status(400).json({success: false, message: "product not found!"})
+	} catch(error){
+		console.error(error);
 	}
-	
 })
 
 
-router.route('/:productId')
-.get(async (req, res) => {
+
+router.param("productId", async (req, res, next, productId) => {
+	const { userId } = req.user;
 	try{
-		const { product } = req;
+		const userWishlist = await Wishlist.findById(userId);
+		const product = userWishlist.wishlist.find(product => product == productId );
+
+		if(!product){
+			return res.status(400).json({success: false, message: "error in getting product"});
+		}
+
+		req.product =  {userWishlist, product};
+		next();
+	}catch{
+		res.status(400).json({ success: false, message: "product not found!" });
+	}
+})
+
+router.route('/:productId')
+.get( async (req, res) => {
+	try{
+		const { product } = req.product;
 		res.json({success: true, product});
 	}catch(error){
 		console.error(error);
@@ -56,12 +73,22 @@ router.route('/:productId')
 
 .delete( async (req, res) => {
 	try{
-		const { product } = req;
-		await product.remove();
-		res.json({success: true, product})
-	}catch(error){
-		console.error(error);
+			const { userWishlist, product } = req.product;
+			const { _id, wishlist } = userWishlist;
+			if(wishlist.length === 1){
+				await userWishlist.remove();
+				res.json({ success: true, product })
+			}else{
+				const filteredWishlist = wishlist.filter(id => id != product);
+				await Wishlist.findByIdAndUpdate({_id }, { wishlist: filteredWishlist });
+				res.json({ success: true, product })
+			}
+			
+	} catch(error){
+			console.error(error);
 	}
 })
 
 module.exports = router;
+
+	
